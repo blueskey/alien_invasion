82efcbfcf59cd8068a7324ca964ba34957064edf
blue_skey <ju.liu@eascs.com>
2022-02-24 10:21:22 +0800

    改变飞船速度

--- ship.py
+++ ship.py
@@ -1,11 +1,14 @@
 import pygame
 
-
 class Ship:
 
     def __init__(self, ai_game):
+
+        """初始化飞船，并设置其初始位置"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+
+        self.settings = ai_game.settings
 
         self.image = pygame.image.load('images/ship.bmp')
 
@@ -13,6 +16,10 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #  rect.x 只能存储整数值，为了让飞船速度可以设置为小数，所以改为在飞船的属性x中存储小数值
+        self.x = float(self.rect.x)
+
+        """移动标志"""
         self.moving_right = False
 
         self.moving_left = False
@@ -22,6 +29,9 @@ class Ship:
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # 根据 self.x 更新 rect 对象
+        self.rect.x = self.x
